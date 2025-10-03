0dbbe9abd372e446e4a51ff50ef1a69f7b8442da
Jayraj Parmar <jayrajsinhp340@gmail.com>
2025-10-03 09:55:59 +0530

    Refactor Sidebar and DashboardLayout for collapsible sidebar functionality

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -10,9 +10,10 @@ import {
   TbTruckLoading,
   TbWallet,
   TbCurrencyRupee,
+  TbLayoutSidebarLeftCollapseFilled,
 } from "react-icons/tb";
 
-function Sidebar() {
+function Sidebar({ collapse, setIsOpen }) {
   const pagesList = [
     {
       key: 1,
@@ -66,29 +67,46 @@ function Sidebar() {
 
   return (
     <>
-      <div className="flex items-center px-4 py-5">
-        <i className="me-4 text-blue-500">
-          <TbTrolley size={35} />
-        </i>
-        <span className="font-bold text-3xl">ERPro</span>
+      <div>
+        <div
+          className={`flex justify-between items-center px-4 py-5 ${
+            collapse ? "justify-center" : ""
+          }`}
+        >
+          <div className="flex">
+            <i className={`${collapse ? "" : "me-4"} text-blue-500`}>
+              <TbTrolley size={35} />
+            </i>
+            {!collapse && <span className="font-bold text-3xl">ERPro</span>}
+          </div>
+
+          <button
+            className="bg-white p-1.5 rounded-md md:hidden block"
+            onClick={() => setIsOpen && setIsOpen(false)}
+          >
+            <i className={`text-gray-900`}>
+              <TbLayoutSidebarLeftCollapseFilled size={25} />
+            </i>
+          </button>
+        </div>
+        <ul className="px-4">
+          {pagesList.map((pageLink) => (
+            <li className="py-1" key={pageLink.key}>
+              <NavLink
+                to={pageLink.path}
+                className={({ isActive }) =>
+                  `flex items-center p-2 rounded-lg hover:bg-gray-800 ${
+                    isActive ? "bg-gray-800 text-blue-500" : ""
+                  } ${collapse ? "justify-center" : ""}`
+                }
+              >
+                <i className={`${collapse ? "" : "me-3"}`}>{pageLink.icon}</i>
+                {!collapse && pageLink.label}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul className="px-4">
-        {pagesList.map((pageLink) => (
-          <li className="py-1" key={pageLink.key}>
-            <NavLink
-              to={pageLink.path}
-              className={({ isActive }) =>
-                `flex items-center p-2 rounded-lg hover:bg-gray-800 ${
-                  isActive ? "bg-gray-800 text-blue-500" : ""
-                }`
-              }
-            >
-              <i className="me-3">{pageLink.icon}</i>
-              {pageLink.label}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
     </>
   );
 }
--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -9,40 +9,67 @@ import {
 
 function DashboardLayout() {
   const [toggle, setToggle] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex h-screen">
-      {/* Sidebar */}
-      <div
-        className={`bg-gray-900 text-white transition-all duration-300
-        ${toggle ? "w-0 overflow-hidden" : "w-64"}`}
-      >
-        <Sidebar />
-      </div>
+    <>
+      <div className="flex h-screen">
+        {/* Sidebar */}
+        <div
+          className={`bg-gray-900 hidden md:block text-white transition-all duration-300
+        ${toggle ? "w-20 overflow-hidden" : "w-64"}`}
+        >
+          <Sidebar collapse={toggle} />
+        </div>
 
-      {/* Main section */}
-      <div className="flex-1 flex flex-col">
-        {/* Navbar */}
-        <header className="border-b-1 border-gray-200 flex items-center justify-between shadow-lg px-4 py-2">
-          <button
-            className="bg-gray-900 p-2 rounded-md text-3xl text-white"
-            onClick={() => setToggle((prev) => !prev)}
-          >
-            {toggle ? (
+        {/* Main section */}
+        <div className="flex-1 flex flex-col">
+          {/* Navbar */}
+          <header className="border-b-1 border-gray-200 flex items-center justify-between shadow-lg px-4 py-2">
+            <button
+              className="bg-gray-900 p-2 rounded-md text-3xl text-white md:block hidden"
+              onClick={() => setToggle((prev) => !prev)}
+            >
+              {toggle ? (
+                <TbLayoutSidebarLeftExpandFilled />
+              ) : (
+                <TbLayoutSidebarLeftCollapseFilled />
+              )}
+            </button>
+
+            <button
+              className="bg-gray-900 p-2 rounded-md text-3xl text-white md:hidden block"
+              onClick={() => setIsOpen((prev) => !prev)}
+            >
               <TbLayoutSidebarLeftExpandFilled />
-            ) : (
-              <TbLayoutSidebarLeftCollapseFilled />
-            )}
-          </button>
-          <Navbar />
-        </header>
+            </button>
+            <Navbar />
+          </header>
 
-        {/* Main content */}
-        <main className="flex-1 p-4 overflow-y-auto bg-gray-100">
-          <Outlet />
-        </main>
+          {/* Main content */}
+          <main className="flex-1 p-4 overflow-y-auto bg-gray-100">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+      <div>
+        {/* Overlay */}
+        <div
+          className={`md:hidden block fixed inset-0 bg-black bg-opacity-50 z-40 transition-opacity ${
+            isOpen ? "opacity-20 visible" : "opacity-0 invisible"
+          }`}
+          onClick={() => setIsOpen(false)}
+        ></div>
+
+        {/* Offcanvas Sidebar */}
+        <div
+          className={`md:hidden block fixed top-0 left-0 h-full w-64 bg-gray-900 text-white shadow-lg z-50 transform transition-transform duration-300 ease-in-out
+        ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
+        >
+          <Sidebar setIsOpen={setIsOpen} />
+        </div>
+      </div>
+    </>
   );
 }
 
